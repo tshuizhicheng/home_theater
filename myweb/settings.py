3b058332fef15665cf05693b58587cac35f33800
sakura <tszcheng@126.com>
2019-05-18 21:22:04 +0800

    test to connect mysqldb

--- myweb/settings.py
+++ myweb/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myweb',
 ]
 ALLOWED_HOSTS = ['*']
 MIDDLEWARE = [
@@ -76,8 +77,15 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'theaterweb',
+        'USER':'root',
+        'PASSWORD':'test123456',
+        'HOST':'',
+        'PORT':'',
+
     }
 }
 
