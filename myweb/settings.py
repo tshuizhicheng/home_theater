74ae97a109e0f0d1f9b6164e4c59437084608a17
sakura <tszcheng@126.com>
2019-05-21 21:52:17 +0800

    change setting.py to remove duplicate movie

--- myweb/settings.py
+++ myweb/settings.py
@@ -31,14 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'movie.apps.MovieConfig',
+    'movie',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myweb',
 ]
 ALLOWED_HOSTS = ['*']
 MIDDLEWARE = [
