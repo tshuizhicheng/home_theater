1685a924aa6be5647fe0770f89938c1a1ed1f232
sakura <tszcheng@126.com>
2019-05-24 21:32:51 +0800

    update setting.py

--- myweb/settings.py
+++ myweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nt7qs0uv40rmpgp2fcd$*l-#h5%v8fi-x735xt)*8lg8kfyl5!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -56,6 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
